e61ee2d392c55b2ef268867dad801be153d6f8a7
Andrii <korrag96@gmail.com>
2018-01-14 23:38:13 +0200

    Implemented DAL interfaces

--- ChatRoom/MainWindow.xaml.cs
+++ ChatRoom/MainWindow.xaml.cs
@@ -24,6 +24,23 @@ namespace ChatRoom
         public MainWindow()
         {
             InitializeComponent();
+            MyControl.SetNewAmountOfMessages(0);
+            MyControl.Avatar.ImageSource = new BitmapImage(new Uri(@"C:\Users\Win7\Desktop\arts\364955.jpg"));
+            MyControl.UnreadedMsgs.Text = "";
+        }
+
+        int amOfMsg = 0;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            amOfMsg += 100;
+            MyControl.SetNewAmountOfMessages(amOfMsg);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            amOfMsg -= 200;
+            MyControl.Msgs = amOfMsg.ToString();
         }
     }
 }
